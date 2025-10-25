56b54f90e2f9df0ba2fb8e5d4d3803ae17acb9fd
gera <168772239+gera444444@users.noreply.github.com>
2025-10-26 02:42:27 +0500

    Update repository.py

--- repository.py
+++ repository.py
@@ -1,51 +1,49 @@
 from sqlalchemy.orm import Session
 from models import User, Product
 from auth import get_password_hash, verify_password
-#подключение к бд через Session 
-class UserRepository:
+
+class Repository:
     def __init__(self, db: Session):
         self.db = db
-    #запрос к бд для получения имени п
+    
+    # User operations
     def get_user_by_username(self, username: str):
         return self.db.query(User).filter(User.username == username).first()
-    #запрос на email
+    
     def get_user_by_email(self, email: str):
         return self.db.query(User).filter(User.email == email).first()
-    #создание нового пользователя 
+    
     def create_user(self, user_data):
-        hashed_password = get_password_hash(user_data.password) #хеширование
+        hashed_password = get_password_hash(user_data.password)
         db_user = User(
             username=user_data.username,
             email=user_data.email,
             hashed_password=hashed_password
         )
         self.db.add(db_user)
-        self.db.commit() #сохранение изменений и обновление данных
+        self.db.commit()
         self.db.refresh(db_user)
         return db_user
-    #вход в систему и проверка на пароль
+    
     def authenticate_user(self, username: str, password: str):
         user = self.get_user_by_username(username)
         if not user or not verify_password(password, user.hashed_password):
-            return False #пароль не вернй 
+            return False
         return user
-#Инициализация объекта класса через Session 
-class ProductRepository:
-    def __init__(self, db: Session):
-        self.db = db
-    #список продуктов с лимитом(пагинация)
+    
+    # Product operations
     def get_products(self, skip: int = 0, limit: int = 100):
         return self.db.query(Product).offset(skip).limit(limit).all()
-    #поиск по id
+    
     def get_product_by_id(self, product_id: int):
         return self.db.query(Product).filter(Product.id == product_id).first()
-    #продукты по категории поиск
+    
     def get_products_by_category(self, category: str):
         return self.db.query(Product).filter(Product.category == category).all()
     
     def create_product(self, product_data):
-        db_product = Product(**product_data.dict())#распаковка для создания модели 
+        db_product = Product(**product_data.dict())
         self.db.add(db_product)
-        self.db.commit() #сохранение изменений и обновление
+        self.db.commit()
         self.db.refresh(db_product)
         return db_product
